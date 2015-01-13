2711ae1fb9dfbcf0ba645964a7b1109defdfe3ba
cowl <cowl@users.noreply.github.com>
2015-01-13 02:26:36 +0100

    The initial framework.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+// INFOMPR 2014-2015, Utrecht University
+// Assignment 2: Implementation of RANSAC
+// Niels Steenbergen, Tom Sýkora
+
+public class Main {
+  public static void main(String[] args){
+    System.out.println("I WANNA BE THE VERY BEST");
+  }
+}
---
+++ src/Point.java
@@ -0,0 +1,8 @@
+// INFOMPR 2014-2015, Utrecht University
+// Assignment 2: Implementation of RANSAC
+// Niels Steenbergen, Tom Sýkora
+
+// Figure 1.5 and 1.6 of small.df
+public class Point {
+  public double x, y;
+}
