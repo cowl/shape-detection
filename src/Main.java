78b4c5e499f6a47f42e08c3cdb7284e7fa575431
sykorto5 <sykorto5@fit.cvut.cz>
2015-01-15 18:21:15 +0100

    minor test change

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // INFOMPR 2014-2015, Utrecht University
 // Assignment 2: Implementation of RANSAC
 // Niels Steenbergen, Tom Sýkora
@@ -6,5 +8,14 @@ public class Main {
   public static void main(String[] args){
 	System.out.println("LETS SHOUT THAT FEELING FROM OUR CHEST");
     System.out.println("WE WANNA BE THE VERY BEST");
+    System.out.println("________________________");
+    
+    System.out.println("Enter img path:");
+	Scanner scan = new Scanner(System.in);
+	String imgpath = scan.next();
+	System.out.println(imgpath);
+	scan.close();
+	
+	System.out.println("END OF PROGRAM");
   }
 }
